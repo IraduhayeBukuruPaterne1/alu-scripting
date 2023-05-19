b448df0e711b40bb8bb8017ce3564844a4a3ed8e
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-05-19 08:12:44 +0000

    paterne

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+if ARGV.empty?
+  puts "Please insert an argument"
+  exit
+end
+input = ARGV[0]
+pattern = /School/
+match = input.scan(pattern)
+if match
+  puts "#{match.join()}"
+else
+  puts ""
+end
